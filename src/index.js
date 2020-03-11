df872ba09bf7338acaf6ff35043399c9960bd028
Maxwell <m4xw3llx@outlook.com>
2020-03-11 19:56:51 +0800

    feat: 增加了给discord发信息的功能

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 "use strict";
 
+// 引用
 const Crawler = require("crawler");
 const cron = require("node-cron");
 const fs = require("fs");
+const axios = require("axios");
+
+// 静态变量
+const LOG_PATH = "./public/project.log";
+const WEBHOOK_URL = "https://discordapp.com/api/webhooks/";
+const WEBHOOK_ID = "687255557689966621";
+const WEBHOOK_TOKEN =
+  "3Vll1V_ZBnfKCZuuibaFK4FFaEvopESLug1adww0_i9FsOAzBmyI_R14RL-3JrkDZJ2-";
 
 // 初始化爬虫
 const c = new Crawler({
@@ -25,17 +34,21 @@ const c = new Crawler({
       } else {
         // 走这里证明有库存了
         writeLog("有库存了，快来抢购");
+        sendMessage();
       }
     }
     done();
   }
 });
 
+/**
+ * 写日志
+ *
+ * @param {*} logText
+ */
 function writeLog(logText) {
-  const logPath = "./public/project.log";
-
   try {
-    const content = fs.readFileSync(logPath);
+    const content = fs.readFileSync(LOG_PATH);
 
     const logDate = new Date();
     const textBuffer = Buffer.concat([
@@ -43,7 +56,7 @@ function writeLog(logText) {
       Buffer.from(`\n${logDate.toString()}\n${logText}`)
     ]);
 
-    fs.writeFile(logPath, textBuffer, error => {
+    fs.writeFile(LOG_PATH, textBuffer, error => {
       if (error) {
         console.error(error);
       }
@@ -53,7 +66,30 @@ function writeLog(logText) {
   }
 }
 
-c.queue("https://www.baidu.com");
+/**
+ * 发送消息
+ *
+ */
+function sendMessage() {
+  axios({
+    method: "POST",
+    url: `${WEBHOOK_URL}${WEBHOOK_ID}/${WEBHOOK_TOKEN}`,
+    data: {
+      content: "搬瓦工终于有货了，快来抢啊！！！"
+    },
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => {
+      console.log("sendMessage -> res", res);
+    })
+    .catch(err => {
+      console.log("sendMessage -> err", err);
+    });
+}
+
+// c.queue("https://www.baidu.com");
 
 // 定时任务 每天9点和21点执行一次
 cron.schedule("* * 9,21 * * *", () => {
