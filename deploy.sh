4d96a37285dbd8dc7d9e8eb5bd785f9ebcd5f3e6
Maxwell <m4xw3llx@outlook.com>
2020-03-12 10:55:36 +0800

    update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 export BANDWAGON_DATA_DIR=~/banwangon-data
 
 mkdir -p "$BANDWAGON_DATA_DIR"
-cd $BANDWAGON_DATA_DIR
+cd "$BANDWAGON_DATA_DIR"
 mkdir public
 cd public
 touch project.log
+cd "$BANDWAGON_DATA_DIR"
 
-docker run -it \ 
-    --mount type=bind,source="$BANDWAGON_DATA_DIR/public",target=/app/public \ 
-    -p 8081:8081 \ 
-    --name bandwagon-crawler
+docker run -d \
+  -it \
+  --name bandwagon-crawler \
+  -p 8081:8081 \
+  --mount type=bind,source="$(pwd)"/public,target=/app/public \
+  bandwagon:latest
