408ce4e1bba6d32d41dc03aab448ae382f5120d3
Maxwell <m4xw3llx@outlook.com>
2020-03-12 10:01:20 +0800

    add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+export BANDWAGON_DATA_DIR=~/banwangon-data
+
+mkdir -p BANDWAGON_DATA_DIR
+cd BANDWAGON_DATA_DIR
+mkdir public
+cd public
+touch project.log
+
+docker run -it \ 
+    --mount type=bind,source="$BANDWAGON_DATA_DIR/public",target=/app/public \ 
+    -p 8081:8081 \ 
+    --name bandwagon-crawler
